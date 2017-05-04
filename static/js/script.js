066794acf026edb42423b63cc304eb7d3c57e765
Cristiane Hdo <cristiane.hdo@gmail.com>
2017-05-04 15:58:51 -0300

    added js to show wheather description and humidity

--- static/js/script.js
+++ static/js/script.js
@@ -3,11 +3,22 @@
 
 
 if (navigator.geolocation) {
-	
+
 	var currentPosition;	
 	navigator.geolocation.getCurrentPosition(function(position) {
-    $("#coords").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
-    // var currentPosition = position;
+    var currentPosition = position;
+    var latitude = position.coords.longitude;
+    var longitude = position.coords.latitude;
+    $("#coords").html("latitude: " + latitude + "<br>longitude: " + longitude);
+
+    $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&APPID=49a7669d694c5f864e8b938ba081ee61", function (data) {
+        var rawJson = JSON.stringify(data);
+        var json = JSON.parse(rawJson);
+        var weather = json.weather[0].description;
+        var humidity = json.main.humidity;
+		$("#weather").html(weather);      
+		$("#humidity").html(humidity+"%"); 
+    });
 
   });
 
