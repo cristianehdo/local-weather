1d67a161260b5d10bba22588db5ffef5f72c670a
Cristiane Hdo <cristiane.hdo@gmail.com>
2017-05-05 20:32:13 -0300

    added icon and a bit of css

--- static/js/script.js
+++ static/js/script.js
@@ -11,21 +11,30 @@ if (navigator.geolocation) {
     var longitude = position.coords.latitude;
     $("#coords").html("latitude: " + latitude + "<br>longitude: " + longitude);
 
-    $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&APPID=49a7669d694c5f864e8b938ba081ee61", function (data) {
+    $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&units=metric&APPID=49a7669d694c5f864e8b938ba081ee61", function (data) {
         var rawJson = JSON.stringify(data);
         var json = JSON.parse(rawJson);
         var weather = json.weather[0].description;
         var humidity = json.main.humidity;
+        var temp = json.main.temp;
 		$("#weather").html(weather);      
 		$("#humidity").html("humidity: "+humidity+"%"); 
+		$("#temp").html(temp + "C");
+
+		var iconweather1 = { "clear sky": "SUN","few clouds": "LIGHTCLOUD", "scattered clouds": "PARTLYCLOUD", "broken clouds": "PARTLYCLOUD", "light rain":"LIGHTRAIN", "shower rain": "LIGHTRAINSUN", "rain": "RAIN", "thunderstorm": "RAINTHUNDER", "snow": "SNOW", "mist": "FOG"}
+		var iconweather = iconweather1[weather];
+		var icon = WeatherIcon.add('icon', WeatherIcon[iconweather], { mode:WeatherIcon.NIGHT, stroke:true, shadow:true, animated:true } );
+    	$("#icon").add(icon);
+
     });
 
   });
 	$.get("http://ipinfo.io", function(response) {
 	var city = response.city;
 	var country= response.country;
-    $("#city").html(city);
+    $("#cityname").html(city);
 }, "jsonp");
+
 
 
 }
